c4a932810d009ab772dd60ce24bef81159604c1f
Bola Ahmed Buari <bola_ahmed2002@yahoo.com>
2020-04-09 00:36:17 +0100

    Remove commented out code

--- src/estimator.js
+++ src/estimator.js
@@ -1,18 +1,3 @@
-// Input
-// {
-//   region: {
-//   name: "Africa",
-//   avgAge: 19.7,
-//   avgDailyIncomeInUSD: 5,
-//   avgDailyIncomePopulation: 0.71
-//   },
-//   periodType: "days",
-//   timeToElapse: 58,
-//   reportedCases: 674,
-//   population: 66622705,
-//   totalHospitalBeds: 1380614
-//  }
-
 import { generateImpactData } from './util/calculations';
 
 const covid19ImpactEstimator = data => {
